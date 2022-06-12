65741b032f20cad4c8e8de396eaad92d2e85d930
VictorEnache <victor22enache@gmail.com>
2022-06-13 00:24:20 +0200

    create cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($fumetti as $fumetto)
-        <h3>{{$fumetto['title']}}</h3>
-    @endforeach
+    <div class="container">
+        <div class="row fumetti">
+            @foreach($fumetti as $fumetto)
+            <div class="col">
+                <div class="card">
+                    <div class="image">
+                        <img src="{{$fumetto['thumb']}}" alt="">
+                    </div>
+                    <!-- /.image -->
+                    <div class="text">
+                    {{$fumetto['series']}}
+                    </div>
+                    <!-- /.text -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+            @endforeach
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,61 +20,61 @@ Route::get('/comics', function () {
 })->name('comics');
 
 Route::get('/characters', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('characters', compact('fumetti', 'links'));
+    return view('characters', compact( 'links'));
 })->name('characters');
 
 Route::get('/movies', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('movies', compact('fumetti', 'links'));
+    return view('movies', compact( 'links'));
 })->name('movies');
 
 Route::get('/tv', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('tv', compact('fumetti', 'links'));
+    return view('tv', compact( 'links'));
 })->name('tv');
 
 Route::get('/games', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('games', compact('fumetti', 'links'));
+    return view('games', compact( 'links'));
 })->name('games');
 
 Route::get('/collectibles', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('collectibles', compact('fumetti', 'links'));
+    return view('collectibles', compact( 'links'));
 })->name('collectibles');
 
 Route::get('/videos', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('videos', compact('fumetti', 'links'));
+    return view('videos', compact( 'links'));
 })->name('videos');
 
 Route::get('/fans', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('fans', compact('fumetti', 'links'));
+    return view('fans', compact( 'links'));
 })->name('fans');
 
 Route::get('/news', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('news', compact('fumetti', 'links'));
+    return view('news', compact( 'links'));
 })->name('news');
 
 Route::get('/shop', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('shop', compact('fumetti', 'links'));
+    return view('shop', compact( 'links'));
 })->name('shop');
 
 Route::get('/', function () {
-    $fumetti = (config('db.fumetti'));
+    
     $links = (config('db.links'));
-    return view('home', compact('fumetti', 'links'));
+    return view('home', compact( 'links'));
 })->name('home');
